d32d966328088ff8d23cfeea6dc40b9336a8f09a
itskristinkaaa <kristinawanna@gmail.com>
2024-01-22 22:52:46 +0200

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Here use name project
+
+### node version 
+*v20.10.0*
+
+### npm version
+*10.2.3*
+
+## Tools
+
+- Gulp
+- SCSS
---
+++ app/js/main.js
@@ -0,0 +1,77 @@
+(() => {
+	const burgerOpen = document.querySelector('.header__box-burger')
+	const burgerClose = document.querySelector('.header__nav-close')
+	const navigation = document.querySelector('.header__nav')
+
+	burgerOpen.addEventListener('click', () => {
+		navigation.classList.add('active')
+	})
+
+	burgerClose.addEventListener('click', () => {
+		navigation.classList.remove('active')
+	})
+})();
+
+(() => {
+	const tabsItem = document.querySelectorAll('.tabs__links-item')
+	const tabsBlock = document.querySelectorAll('.tabs__content-item')
+	
+	tabsItem.forEach((tab, index) => {
+		tab.addEventListener('click', () => {
+			tabsBlock.forEach((content) => {
+				content.classList.remove('active')
+			})
+			tabsItem.forEach((tab) => {
+				tab.classList.remove('active')
+			})
+			tabsItem[index].classList.add('active')
+			tabsBlock[index].classList.add('active')
+		})
+	})
+})();
+
+const swiper = new Swiper('.team__swiper', {
+  loop: true,
+	spaceBetween: 30,
+	grabCursor: true,
+	// autoplay: {
+	// 	delay: 1000,
+	// },
+	// effect: 'cube',
+  // cubeEffect: {
+  //   slideShadows: false,
+  // },
+
+	// effect: 'fade',
+  // fadeEffect: {
+  //   crossFade: true
+  // },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+		clickable: true
+  },
+});
+
+// POPUP
+(() => {
+	const servicesBtn = document.querySelector('.services__btn')
+	const formPopup = new Popup({
+		id: "form__popup",
+		title: "We would like to help you",
+		content: `<form>
+								<input type="email">
+								<textarea></textarea>
+							</form>`,
+		borderRadius: '12px',
+		titleMargin: "0",
+	});
+
+	console.log(formPopup)
+
+	servicesBtn.addEventListener('click', () => {
+		formPopup.show()
+	})
+
+})();
